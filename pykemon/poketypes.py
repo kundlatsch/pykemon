01f8fd3409565fabda0ba3cf292a004a1d1b8181
kundlatsch <gustavo.kundlatsch@gmail.com>
2020-01-01 13:58:21 -0300

    Change Stats NamedTuple to Dataclass to allow mutation

--- pykemon/poketypes.py
+++ pykemon/poketypes.py
@@ -1,11 +1,13 @@
 import pokepy
+from dataclasses import dataclass
 from typing import NamedTuple
 
 class VersionedMove(NamedTuple):
     move: pokepy.resources_v2.MoveResource
     version: pokepy.resources_v2.PokemonMoveSubResource
 
-class Stats(NamedTuple):
+@dataclass
+class Stats():
     speed: int
     special_defense: int
     special_attack: int
